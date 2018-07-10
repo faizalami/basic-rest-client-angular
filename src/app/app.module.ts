f77bb06cb5d5c77e5b22c837af259c95b7978b48
Faizal Amiruddin <f.a.faizal.amiruddin@gmail.com>
2018-07-10 19:22:20 +0700

    add name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { FormComponent } from './form/form.component';
-import { CreateComponent } from './create/create.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 
@@ -18,7 +17,6 @@ import {FormsModule} from "@angular/forms";
     NavbarComponent,
     HomeComponent,
     FormComponent,
-    CreateComponent,
   ],
   imports: [
     BrowserModule,
